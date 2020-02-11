c03aebbd257fa295fdb72613de538b12e3abc935
Dev Daksan <dev.91096@gmail.com>
2020-02-11 19:14:07 +0530

    Added Comments for functions

--- controllers/utils.go
+++ controllers/utils.go
@@ -1,11 +1,13 @@
 package controllers
 
 import (
-	"github.com/R3l3ntl3ss/Meme_Api/models"
 	"math/rand"
 	"time"
+
+	"github.com/R3l3ntl3ss/Meme_Api/models"
 )
 
+// GetRandomN : Get a Random Number to a particular range n
 func GetRandomN(n int) (num int) {
 	rand.Seed(time.Now().Unix())
 	num = rand.Int() % n
@@ -13,6 +15,7 @@ func GetRandomN(n int) (num int) {
 	return
 }
 
+// GetNRandomMemes : Get N no. of random memes from a list of Memes
 func GetNRandomMemes(memes []models.Meme, n int) []models.Meme {
 	rand.Seed(time.Now().UnixNano())
 	rand.Shuffle(len(memes), func(i, j int) { memes[i], memes[j] = memes[j], memes[i] })
